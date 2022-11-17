efef3099d6073c3862753219392f015e90be11a5
Andréa Masselot <andrea.nicolas128@gmail.com>
2022-11-17 12:38:46 +0100

    CSS on characterId page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Characters from "./pages/Characters";
 import Header from "./components/Header";
 import Comics from "./pages/Comics";
+import CharacterComs from "./pages/CharacterComs";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Characters />} />
         <Route path="/comics" element={<Comics />} />
+        <Route path="/comics/:characterId" element={<CharacterComs />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/CharacterComs.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import Card from "../components/Card";
+
+const CharacterComs = () => {
+  const { characterId } = useParams();
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:4000/comics/${characterId}`
+        );
+        // console.log(response.data);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, [characterId]);
+
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (
+    <>
+      <section className="heroes-identity">
+        <figure className="id-picture">
+          <img
+            src={`${data.thumbnail.path}.${data.thumbnail.extension}`}
+            alt="hero id photos"
+          />
+        </figure>
+        <div className="details-id">
+          <h2>{data.name}</h2>
+          <p>{data.description}</p>
+        </div>
+      </section>
+      <div className="container">
+        <div className="comics-container">
+          {data.comics.map((elem, index) => {
+            return (
+              <Card
+                key={elem._id}
+                name={elem.title}
+                description={elem.description}
+                image={`${elem.thumbnail.path}.${elem.thumbnail.extension}`}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CharacterComs;
